9fc6e0ef6f33c28a154cb3d4b493e856f2076ba4
Javier Zapata <javierzapata82@gmail.com>
2026-05-01 10:04:29 +0200

    fix: clipboard through ssh

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,10 +1,24 @@
 require "nvchad.options"
 
--- add yours here!
-
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt ='both'
 
 o.scrolloff = 10
 o.number = true
 o.relativenumber = true
+
+-- Enable clipboard through SSH
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+
+vim.opt.clipboard = "unnamedplus"
