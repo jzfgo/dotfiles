3281c82bd784db7a767bb125a46a2e4c12ea0bef
Javier Zapata <javierzapata82@gmail.com>
2026-05-11 13:53:15 +0200

    feat(nvim): set up luasnip plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -109,4 +109,14 @@ return {
       },
     },
   },
+
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    build = "make install_jsregexp",
+    config = function()
+      require "nvchad.configs.luasnip"
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 }
